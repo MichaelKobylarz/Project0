800e2c742323b0af27931aa613ce9af3d37f3a42
MichaelKobylarz <michalkobylarz@gmail.com>
2024-09-04 10:08:51 +0200

    db bangla

--- orm/viewer/views.py
+++ orm/viewer/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-# def hello(request):
-#     return HttpResponse('Hello, world!')
 def hello(request):
+    return HttpResponse('Hello, world!')
+
+def hello(request, s):
     s = request.GET.get('s', '')
     return HttpResponse(f'Hello, {s} word!')
